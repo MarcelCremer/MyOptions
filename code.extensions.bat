9d9da3cacfa8bba51a593fd90a3e71d62d0c639a
Marcel Cremer <marcelcremer@users.noreply.github.com>
2018-04-27 07:59:35 +0200

    Added Prettier, new lintings and vscode-extensions

---
+++ code.extensions.bat
@@ -0,0 +1,33 @@
+code --install-extension aeschli.vscode-css-formatter
+
+code --install-extension Angular.ng-template
+
+code --install-extension christian-kohler.path-intellisense
+
+code --install-extension dbaeumer.jshint
+
+code --install-extension DSKWRK.vscode-generate-getter-setter
+
+code --install-extension eamodio.gitlens
+
+code --install-extension EditorConfig.EditorConfig
+
+code --install-extension eg2.tslint
+
+code --install-extension esbenp.prettier-vscode
+
+code --install-extension IBM.output-colorizer
+
+code --install-extension MattiasPernhult.vscode-todo
+
+code --install-extension msjsdiag.debugger-for-chrome
+
+code --install-extension patrys.vscode-code-outline
+
+code --install-extension PeterJausovec.vscode-docker
+
+code --install-extension shd101wyy.markdown-preview-enhanced
+
+code --install-extension steoates.autoimport
+
+code --install-extension Tyriar.lorem-ipsum
